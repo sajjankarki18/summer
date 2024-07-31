4bf734abdf302a26b547aa9260031631dd007862
sajan <sajjankarki10@gmail.com>
2024-07-31 21:03:56 +0530

    project added

--- upload_your_images/settings.py
+++ upload_your_images/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'upload_your_images.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'upload_photos',
+        'USER': 'postgres',
+        'PASSWORD':'sajan123',
+        'HOST': 'localhost'
     }
 }
 
